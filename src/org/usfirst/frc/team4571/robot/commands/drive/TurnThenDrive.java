1d00479d3553ff05ce4234c89b8eb86e5200f482
mxhim <mahim216@gmail.com>
2017-02-11 13:31:51 -0500

    Fixed command group

--- src/org/usfirst/frc/team4571/robot/commands/drive/TurnThenDrive.java
+++ src/org/usfirst/frc/team4571/robot/commands/drive/TurnThenDrive.java
@@ -12,6 +12,8 @@ public class TurnThenDrive extends CommandGroup {
     public TurnThenDrive(double angle, double distanceInInches) {
         requires(Robot.TANK_DRIVE_SUBSYSTEM);
         addSequential(new TurnDegreesCommand(angle));
+        Robot.TANK_DRIVE_SUBSYSTEM.getTurnController().disable();
+        Robot.TANK_DRIVE_SUBSYSTEM.reset();
         addSequential(new AutoDriveCommand(distanceInInches, 0.0));
     }
 }
